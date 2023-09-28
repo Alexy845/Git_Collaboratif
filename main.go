2203457b78a55c04827aa605ca2c0e21c3b344bd
llyse <lyse.lebourcq@ynov.com>
2023-09-28 12:05:24 +0200

    morpion

--- main.go
+++ main.go
@@ -74,8 +74,26 @@ func IsValidMove(move [2]int) bool {
 }
 
 func hasWon(player string) bool {
-	// Implémentez la logique de vérification de victoire ici
-	// Vérifiez les lignes, les colonnes et les diagonales
+	// Vérification des lignes
+	for i := 0; i < 3; i++ {
+		if board[i][0] == player && board[i][1] == player && board[i][2] == player {
+			return true
+		}
+	}
+
+	// Vérification des colonnes
+	for j := 0; j < 3; j++ {
+		if board[0][j] == player && board[1][j] == player && board[2][j] == player {
+			return true
+		}
+	}
+
+	// Vérification des diagonales
+	if (board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+		(board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+		return true
+	}
+
 	return false
 }
 
